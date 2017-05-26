29db3ff82c81240d498365688cecf4e60e5eb1cd
Frank Dabek <fdabek@gmail.com>
2017-05-25 23:24:48 -0400

    to app engine

--- coping-pattern.go
+++ coping-pattern.go
@@ -5,10 +5,22 @@ import (
 	"github.com/llgcode/draw2d/draw2dimg"
 	"image"
 	"image/color"
+	"image/png"
 	"math"
+	"net/http"
+	"strconv"
 )
 
-func main() {
+func init() {
+    http.HandleFunc("/png", handler)
+}
+
+func handler(writer http.ResponseWriter, req *http.Request) {
+	if (req.ParseForm() != nil) {
+		fmt.Fprint(writer, "Error parsing request")
+		return
+	}
+
 	dest := image.NewRGBA(image.Rect(0, 0, 1000, 1000))
 	gc := draw2dimg.NewGraphicContext(dest)
 	gc.SetStrokeColor(color.RGBA{0x44, 0x44, 0x44, 0xff})
@@ -19,19 +31,20 @@ func main() {
 
 	gc.MoveTo(0, kVertOffset)
 
-	R := 1.5
-	r := 1.125
-	phi := 72.0 / 360.0 * 2*kPi
+	R,_ := strconv.ParseFloat(req.FormValue("R"), 64)
+	r,_ := strconv.ParseFloat(req.FormValue("r"), 64)
+	phi_deg,_ := strconv.ParseFloat(req.FormValue("phi"), 64)
+	phi := phi_deg / 360.0 * 2*kPi
 	cosphi := math.Cos(phi)
 
 	var w float64 = 0
 	for ; w < r*2*kPi; w += 0.1 {
                 x_disp := (r*math.Sin(w/r))
-		d := R - math.Sqrt(R*R - x_disp*x_disp) - (r - r*math.Cos(w/r))*cosphi
+		d := R - math.Sqrt(R*R - x_disp*x_disp) + (r - r*math.Cos(w/r))*cosphi
 		fmt.Println(w * 100, kVertOffset + d * 100)
 		gc.LineTo(w * 100, kVertOffset + d*100)
 	}
 	gc.Stroke()
 
-	draw2dimg.SaveToPngFile("cope.png", dest)
+	png.Encode(writer, dest)
 }
